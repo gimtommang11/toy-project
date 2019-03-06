fb5647acca3c417c5074539b55569348589608b7
gimtommang <sh20058@naver.com>
2019-03-06 20:11:16 +0900

    퍼블리싱 끝

--- mail.js
+++ mail.js
@@ -1,6 +1,14 @@
 const tab = document.getElementById("tab");
 const inbox = document.getElementById("inbox")
 const list = document.getElementById('list')
+const inboxName = document.getElementById('inboxName');
+//--글 써주는곳
+const tagName = document.getElementsByClassName('tagName');
+const mailTitle = document.getElementById('mailTitle');
+const userName = document.getElementById('userName');
+const mailDate = document.getElementById('mailDate');
+const content = document.getElementById('content');
+
 let lists = []
 let tabs = [];
 
@@ -16,43 +24,22 @@ for(let i = 0; i < tab.children.length;i++ ){
 }
 
 function changePos(order){
-/*    if(order == 0){
-        inbox.innerText = "case0"
+   if(order == 0){
+        inboxName.innerText = "Home"
         console.log("0 is clicked")
     }
     else if(order == 1){
-        inbox.innerText = "case1"
+        inboxName.innerText = "Customers"
         console.log("1 is clicked")
     }
     else if(order == 2){
-        inbox.innerText ='case2'
+        inboxName.innerText ='Inbox'
         console.log("2 is clicked")
-        inbox.innerHTML = `
-        <div id = compose>compose</div>
-        <div id = menu></div>       <!--inbox/tag-->
-        <form ><input type="text" id="search" value="search"></form>
-        <div id = userList>
-            <ul id = list>
-                <li>a</li>
-                <li>b</li>
-                <li>c</li>
-                <li>d</li>
-            </ul>
-       </div>
-        <div id="mainText">
-            <div id="nameBox">
-                <p id="name"></p>
-            </div>
-            <div id = contentBox>
-                <p id=content></p>
-            </div>
-        </div>
-        `;
     }
     else{
-        inbox.innerText = "case3"
+        inboxName.innerText='Settings'
         console.log("3 is clicked")
-    }*/
+    }
 }
 
 function clear_(act){
@@ -68,24 +55,32 @@ function clear_(act){
     }
 }
 
-
 //------------------------------------------
 for(let i = 0; i<list.children.length;i++){
     lists[i] = list.children[i]
     lists[i].addEventListener("click", function(e){
         if(e.target == e.currentTarget){
-        clear_('islist'); 
-        e.target.classList.add('active');}
+            clear_('islist'); 
+            e.target.classList.add('active');
+            
+        }
     })
 }
 
 const listobj =[   
     {
-        names : 'asd',
-        content : 'adf'
+        names :'David James',
+        content : 'Hi David,<br>Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum assumenda ipsam magnam cum possimus. Laudantium nulla amet tempore excepturi id expedita dolorum quisquam deserunt. Odit vel sint dolor eos. Ea blanditiis animi. Quibusdam unde unde. Perspiciatis vel pariatur qui. Deleniti omnis est quae. Laboriosam numquam amet aliquid.<br> Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum assumenda ipsam magnam cum possimus. Laudantium nulla amet tempore excepturi id expedita dolorum quisquam deserunt. Odit vel sint dolor eos. Ea blanditiis animi. Quibusdam unde unde. Perspiciatis vel pariatur qui. Deleniti omnis est quae. Laboriosam numquam amet aliquid.Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum<br> Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum assumenda ipsam magnam cum possimus. Laudantium nulla amet tempore excepturi id expedita dolorum ',
+        tag : 'Design',
+        title : 'Protective Preventative Maintenance',
+        maildate :'Jan 5'
     },
     {
-        names : 'qwer',
-        content : 'qwe' 
+        names : 'Tony Stark',
+        content : 'Hi David,<br>Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum assumenda ipsam magnam cum possimus. Laudantium nulla amet tempore excepturi id expedita dolorum quisquam deserunt. Odit vel sint dolor eos. Ea blanditiis animi. Quibusdam unde unde. Perspiciatis vel pariatur qui. Deleniti omnis est quae. Laboriosam numquam amet aliquid.<br> Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum assumenda ipsam magnam cum possimus. Laudantium nulla amet tempore excepturi id expedita dolorum quisquam deserunt. Odit vel sint dolor eos. Ea blanditiis animi. Quibusdam unde unde. Perspiciatis vel pariatur qui. Deleniti omnis est quae. Laboriosam numquam amet aliquid.Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum<br> Iste minus et. Non necessitatibus ut est est id amet. Officiis sequi dolorum assumenda ipsam magnam cum possimus. Laudantium nulla amet tempore excepturi id expedita dolorum ',
+        tag : 'Sales',
+        title : 'Does this mail service work well?',
+        maildate :'Jan 4'
     }
 ]
+
